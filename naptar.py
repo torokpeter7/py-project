8510be90254724264ee7dea22eb0e509c33a5d16
RafaelAlex123 <szekeresrafaelalex@turr.hu>
2023-05-18 11:21:44 +0200

    menu berakasa a python fileokba

--- naptar.py
+++ naptar.py
@@ -1,6 +1,39 @@
 from tkinter import *
 import calendar
+root = Tk()
 
+menubar = Menu(root)
+
+Kezdőoldalmenu = Menu(menubar, tearoff=0)
+
+menubar.add_cascade(label="Kezdőooldal")
+
+menubar.add_cascade(label="Akasztófa")
+
+menubar.add_cascade(label="Valuta Váltó")
+
+menubar.add_cascade(label="Valorant Line-up Guide")
+
+menubar.add_cascade(label="QR kód generátor")
+
+menubar.add_cascade(label="Jelszó generátor")
+
+
+aksztofa=menubar.add_cascade(label="Iraki óra")
+
+akszto=Menu(menubar, tearoff=0)
+
+valuta=Menu(menubar, tearoff=0)
+
+valolineup=Menu(menubar, tearoff=0)
+
+qrkodgen=Menu(menubar, tearoff=0)
+
+jelszogen=Menu(menubar, tearoff=0)
+
+naptar=Menu(menubar, tearoff=0)
+
+irakiora=Menu(menubar, tearoff=0)
 def showCal():
     uj_ablak=Tk()
     uj_ablak.config (background="white")
--- valorantguide.py
+++ valorantguide.py
@@ -1,2 +1,36 @@
 from tkinter import *
 
+root = Tk()
+
+menubar = Menu(root)
+
+Kezdőoldalmenu = Menu(menubar, tearoff=0)
+
+menubar.add_cascade(label="Kezdőooldal")
+
+menubar.add_cascade(label="Akasztófa")
+
+menubar.add_cascade(label="Valuta Váltó")
+
+
+menubar.add_cascade(label="QR kód generátor")
+
+menubar.add_cascade(label="Jelszó generátor")
+
+menubar.add_cascade(label="Naptár")
+
+aksztofa=menubar.add_cascade(label="Iraki óra")
+
+akszto=Menu(menubar, tearoff=0)
+
+valuta=Menu(menubar, tearoff=0)
+
+valolineup=Menu(menubar, tearoff=0)
+
+qrkodgen=Menu(menubar, tearoff=0)
+
+jelszogen=Menu(menubar, tearoff=0)
+
+naptar=Menu(menubar, tearoff=0)
+
+irakiora=Menu(menubar, tearoff=0)
